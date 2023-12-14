83261bc744ec4070f4a96a4df1ebaaa69848f74c
Mitchell Stave <mitchjohnston99@gmail.com>
2023-12-13 18:27:26 -0800

    started interface

---
+++ src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import { Tab } from "@headlessui/react";
+
+export default function Nav(props: any) {
+  return (
+    <Tab.Group>
+      <Tab.List className="tabs tabs-lifted tabs-lg">
+        <Tab className="tab tab-active">Main</Tab>
+        <Tab className="tab">Tab 2</Tab>
+        <Tab className="tab">Tab 3</Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>Content 3</Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+  );
+}
