b3c626e949a4ccd629a07241a6cc119b7ab53b06
Mitchell Stave <mitchjohnston99@gmail.com>
2024-03-22 15:08:34 -0700

    fixed line issues

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -19,7 +19,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-gray-900 transition-all dark:bg-gray-50"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
